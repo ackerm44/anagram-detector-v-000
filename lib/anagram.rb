1d10562bc85a6a9b91e58b0edc0fd79ed82987d6
Amanda Ackerman <ackerm44@gmail.com>
2017-10-31 15:05:49 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,12 +2,20 @@ class Anagram
   attr_accessor :word
 
   def initialize(word)
-    @word = word
-    puts word.split.sort!
-
+    @word = word.split("").sort!
   end
-
+  
+  
   def match(word_array)
+    new_array = []
+    word_array.each do |word|
+      sorted = word.split("").sort
+      new_array << sorted
+    end
+    new_array.each do |word|
+      new_array.index(word) if word == @word
+    end
+    result = word_array[new_array.index(word)]
   end
 
 
