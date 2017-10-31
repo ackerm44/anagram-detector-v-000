911626645704083f56946fb88e432552e6e7dbb1
Amanda Ackerman <ackerm44@gmail.com>
2017-10-31 16:01:55 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,25 +1,25 @@
+
 class Anagram
   attr_accessor :word
 
   def initialize(word)
-    @word = word.split("").sort!
+    @word = word.split("").sort.join
   end
-
-
+  
+  
   def match(word_array)
     new_array = []
     result = []
     word_array.each do |word|
-      sorted = word.split("").sort
-      new_array << sorted
+      new_array << word.split("").sort.join
     end
-    new_array.each do |word|
+    new_array.each_with_index do |word, index|
       if word == @word
-        new_array.index(word)
-        result << word_array[new_array.index(word)]
+        result << word_array[index]
       end
     end
     result
+
   end
 
 
