85ff458fdfbe12c1e3d9ab0b575ec43f2bd80905
Amanda Ackerman <ackerm44@gmail.com>
2017-10-31 15:12:07 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,24 +4,23 @@ class Anagram
   def initialize(word)
     @word = word.split("").sort!
   end
-
-
+  
+  
   def match(word_array)
     new_array = []
+    result = []
     word_array.each do |word|
       sorted = word.split("").sort
       new_array << sorted
     end
     new_array.each do |word|
       if word == @word
-        #new_array.index(word)
-        result = word_array[new_array.index(word)]
-        result
-      else
-        result = []
+        new_array.index(word) 
+        result << word_array[new_array.index(word)]
       end
-      result
     end
-
+    result 
   end
+
+
 end
